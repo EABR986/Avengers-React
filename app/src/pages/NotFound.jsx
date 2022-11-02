1c6434c8a53fb4985b6bf9975da993e82842fa8c
EABR986 <eduardoblanco198@gmail.comndgit config --global user.email eduardoblanco1987@gmail.com>
2022-11-02 04:57:09 +0100

    feat:not found page

--- app/src/pages/NotFound.jsx
+++ app/src/pages/NotFound.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>404 - No existe la ruta!</div>
+    <>
+    <div>
+      <h1>404 Page Not Found</h1>
+      <h3>HYDRA is currently attacking this page!</h3>
+      <p className="card-body">Check that you typed the address correctly, go back to your previous page or try using our site search to find something specific. </p>
+    </div>
+    <div>
+        <img src="https://i.annihil.us/u/prod/marvel/html_pages_assets/error-pages/prod/captain-america-char.4b7f4c07.png" alt="HYDRA" />
+      </div>
+      </>
   )
 }
 
